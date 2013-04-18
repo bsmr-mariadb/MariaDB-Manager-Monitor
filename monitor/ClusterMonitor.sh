24fb28ab4b4985e3d8bad1fbd4412e682cfeeb56
Timofey Turenko <timofey.turenko@skysql.com>
2013-04-18 21:43:49 +0000

    ClusterMonitor.sh typo fix

--- monitor/ClusterMonitor.sh
+++ monitor/ClusterMonitor.sh
@@ -1,3 +1,4 @@
+
 #!/bin/sh
 . /usr/local/skysql/skysql_aws/skysql.config
 cd /usr/local/skysql/monitor
@@ -6,4 +7,4 @@ PATH=${PATH}:/usr/sbin
 
 jars_path=`find /usr/local/skysql/skysql_aws/*.jar | tr '\n' ':' `
 
-nohup java -classpath "jars_path"ClusterMonitor.jar com.skysql.monitor.ClusterMonitor $system_id /usr/local/skysql/SQLite/AdminConsole/admin > /var/log/SkySQL-ClusterMonitor.log 2>&1 &
+nohup java -classpath "$jars_path"ClusterMonitor.jar com.skysql.monitor.ClusterMonitor $system_id /usr/local/skysql/SQLite/AdminConsole/admin > /var/log/SkySQL-ClusterMonitor.log 2>&1 &
