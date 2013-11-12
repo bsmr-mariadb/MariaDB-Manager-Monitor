fe9dcc68dc5ecd60d90cdd6f93d7392b7cbb123d
Massimo Siani <massimo.siani@skysql.com>
2013-11-12 10:38:22 -0500

    V107

--- ClusterMonitor/src/com/skysql/monitor/ClusterMonitor.java
+++ ClusterMonitor/src/com/skysql/monitor/ClusterMonitor.java
@@ -93,7 +93,7 @@ public class ClusterMonitor extends Thread {
 			verbose = true;
 		}
 
-		Logging.info("Starting ClusterMonitor v1.7-106");
+		Logging.info("Starting ClusterMonitor v1.7-107");
 		Logging.info("==============================");
 		
 		if (args[off].equalsIgnoreCase("all"))
